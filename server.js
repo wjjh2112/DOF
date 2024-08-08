990b0e861e90241ed68f131a533eabb8494d6c70
Waji <wajihahzaidi2@gmail.com>
2024-08-08 18:35:58 +0800

    Update

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://adminDOF:!sbdDOF2021080824!@13.215.209.29:27017/DOF', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    authSource: 'DOF'
+mongoose.connect('mongodb://admin:!sbdDOF2021080824!@13.215.209.29:27017/DOF', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  authSource: 'admin' // the database where the user is created
 }).then(() => {
-    console.log('Connected to MongoDB DOF');
+  console.log('Connected to MongoDB');
 }).catch(err => {
-    console.error('Failed to connect to MongoDB DOF', err);
+  console.error('Failed to connect to MongoDB', err);
 });
 
 // Middleware
@@ -23,12 +23,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files
+// Serve static files (from root directory)
 app.use(express.static(path.join(__dirname)));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/login', (req, res) => {
