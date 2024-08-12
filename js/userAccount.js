f01902d3e602e6e4f8667ef542f186dd0930afc8
Waji <wajihahzaidi2@gmail.com>
2024-08-12 19:59:08 +0800

    Update logout functionality

--- js/userAccount.js
+++ js/userAccount.js
@@ -1,18 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const userData = JSON.parse(sessionStorage.getItem('userData'));
 
-    if (userData) {
-        // Control visibility based on user type
-        if (userData.usertype === 'Viewer') {
-            document.getElementById('sidebar-users-item').style.display = 'none';
-            document.getElementById('mobile-users-item').style.display = 'none';
-        }
-
-        // Populate user profile fields
-        document.getElementById('firstNameInput').value = userData.firstname;
-        document.getElementById('lastNameInput').value = userData.lastname;
-        document.getElementById('emailInput').value = userData.email;
-    } else {
+    if (!userData) {
         // Redirect to login page if user data is not found
         window.location.href = '/login';
     }
