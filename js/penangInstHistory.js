2fef5d43aafdc252516534568136887a220f1dd9
Waji <wajihahzaidi2@gmail.com>
2024-08-26 10:32:54 +0800

    Test data display

--- js/penangInstHistory.js
+++ js/penangInstHistory.js
@@ -18,6 +18,38 @@ $(function() {
         autoUpdateInput: false
     });
 
+    // Helper function to map dropdown value to tank identifier
+    function mapDropdownValueToTank(dropdownValue) {
+        switch (dropdownValue) {
+            case 'Tank 1':
+                return 'PEN1';
+            case 'Tank 2':
+                return 'PEN2';
+            case 'Tank 3':
+                return 'PEN3';
+            case 'All Tanks':
+            default:
+                return '';  // Return empty to indicate all tanks
+        }
+    }
+
+    // Helper function to map tank identifier to device ID (if needed)
+    function mapTankToDeviceID(tankID) {
+        switch (tankID) {
+            case 'PEN1_DO':
+            case 'PEN1_PH':
+                return 'Tank 1';
+            case 'PEN2_DO':
+            case 'PEN2_PH':
+                return 'Tank 2';
+            case 'PEN3_DO':
+            case 'PEN3_PH':
+                return 'Tank 3';
+            default:
+                return 'Unknown Tank';
+        }
+    }
+
     function fetchDataAndDisplayTable() {
         const tank = $('#dropdownPenLogger').val();
         const dateRange = $('#penDateRangePicker').val();
