1be3c6279fa767ccecb547fd3ce0526479e2ba64
Waji <wajihahzaidi2@gmail.com>
2024-09-06 16:27:53 +0800

    Update remove dummy data

--- js/penangInstHistory.js
+++ js/penangInstHistory.js
@@ -33,65 +33,14 @@ $(function() {
         }
     }
 
-    function fetchDataAndDisplayTable() {
-        const tank = $('#dropdownPenLogger').val();
-        const dateRange = $('#penDateRangePicker').val();
-        let startDate = '';
-        let endDate = '';
-
-        if (dateRange) {
-            const dates = dateRange.split(' - ');
-            startDate = dates[0];
-            endDate = dates[1];
-        }
-
-        $.ajax({
-            url: '/api/data',
-            method: 'GET',
-            data: {
-                tank: mapDropdownValueToTank(tank),
-                startDate: startDate,
-                endDate: endDate
-            },
-            success: function(data) {
-                console.log('Data received from server:', data);
-                const tbody = $('#penHistoryBody');
-                tbody.empty(); // Clear existing rows
-
-                if (data.length === 0) {
-                    tbody.append('<tr><td colspan="5" style="text-align: center;">No records found</td></tr>');
-                } else {
-                    data.forEach(record => {
-                        const date = moment(record.timestamp).format('DD/MM/YYYY');
-                        const time = moment(record.timestamp).format('HH:mm:ss');
-                        const tankName = record.collection.split('_')[0]; // Extract 'PEN1', 'PEN2', etc.
-
-                        tbody.append(`
-                            <tr>
-                                <td>${tankName}</td>
-                                <td>${date}</td>
-                                <td>${time}</td>
-                                <td>${record.payload}</td>
-                                <td>${record.payload}</td>
-                            </tr>
-                        `);
-                    });
-                }
-            },
-            error: function(err) {
-                console.error('Error fetching data:', err);
-            }
-        });
-    }
-
     // Event listeners for dropdown and date range picker
-    $('#dropdownPenLogger').change(fetchDataAndDisplayTable);
-    $('#penDateRangePicker').on('apply.daterangepicker', fetchDataAndDisplayTable);
-    $('#penDateRangePicker').on('cancel.daterangepicker', function() {
-        $(this).val('');
-        fetchDataAndDisplayTable();
-    });
+    // $('#dropdownPenLogger').change(fetchDataAndDisplayTable);
+    // $('#penDateRangePicker').on('apply.daterangepicker', fetchDataAndDisplayTable);
+    // $('#penDateRangePicker').on('cancel.daterangepicker', function() {
+    //     $(this).val('');
+    //     fetchDataAndDisplayTable();
+    // });
 
-    // Fetch data on page load
-    fetchDataAndDisplayTable();
+    // // Fetch data on page load
+    // fetchDataAndDisplayTable();
 });
