7ff3ba4878ae102c671b45e00966a9b591459ada
Rachel Marvin Wan <121346040+RachelMarvinWan@users.noreply.github.com>
2024-09-13 18:46:31 +0800

    Update history.js

--- js/history.js
+++ js/history.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         if (userData.userOrg === 'Admin'){
-            document.getElementById('Admin-History').style.display = 'inline';
+            document.getElementById('Admin-History').style.display = 'inline-flex';
         }
         
     } 
